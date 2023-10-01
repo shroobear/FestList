1f3b3912372a7968108cf9bf0ed117dfb0098006
Stanley Shrewsbery <homestanrunner@gmail.com>
2023-09-30 20:36:35 -0400

    adds cascading deletion rules for ORM entries

--- server/models.py
+++ server/models.py
@@ -18,9 +18,9 @@ class Festival(db.Model):
     date = db.Column(db.Date, nullable=False)
     website = db.Column(db.String)
     # Many to many relationship with Artist through lineups table
-    lineup = db.relationship('Lineup', back_populates='festival')
+    lineup = db.relationship('Lineup', cascade="all, delete", back_populates='festival')
 
-    user_festival = db.relationship('User_Festival', back_populates='festival')
+    user_festival = db.relationship('User_Festival', cascade="all, delete", back_populates='festival')
 
 class Artist(db.Model):
     __tablename__ = 'artists'
@@ -28,24 +28,12 @@ class Artist(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     # Many to many relationship with Festival through lineup table
-    lineup = db.relationship('Lineup', back_populates='artist')
+    lineup = db.relationship('Lineup', cascade="all, delete", back_populates='artist')
     # Many to many relationship with Song through Song_Artist table
-    song_artists = db.relationship('Song_Artist', back_populates='artist')
+    song_artists = db.relationship('Song_Artist', cascade="all, delete-orphan", back_populates='artist')
     #  Define many to many relationship with User through Favorite table
-    favorites = db.relationship('Favorite', back_populates='artist')
+    favorites = db.relationship('Favorite', cascade="all, delete", back_populates='artist')
 
-
-class Lineup(db.Model):
-    __tablename__ = 'lineups'
-    # Ensure that all Artist Festival combinations are unique within the join table
-    __table_args__ = (UniqueConstraint('festival_id', 'artist_id', name='unique_lineup'),)
-
-    id = db.Column(db.Integer, primary_key=True)
-    festival_id = db.Column(db.Integer, db.ForeignKey('festivals.id'), nullable=False)
-    artist_id = db.Column(db.Integer, db.ForeignKey('artists.id'), nullable=False)
-
-    festival = db.relationship('Festival', back_populates='lineup')
-    artist = db.relationship('Artist', back_populates='lineup')
 
 class Song(db.Model):
     __tablename__ = 'songs'
@@ -54,20 +42,7 @@ class Song(db.Model):
     name = db.Column(db.String, nullable=False)
     spotify_id = db.Column(db.String)
     # Many to many relationship with Artist through Song_Artist table
-    song_artists = db.relationship('Song_Artist', back_populates='song')
-
-class Song_Artist(db.Model):
-    __tablename__ = 'song_artists'
-    # Ensure all combinations of Artists and songs are unique to prevent duplicates
-    __table_args__ = (UniqueConstraint('song_id', 'artist_id', name='unique_collaboration'),)
-
-    id = db.Column(db.Integer, primary_key=True)
-    song_id = db.Column(db.Integer, db.ForeignKey('songs.id'), nullable=False)
-    artist_id = db.Column(db.Integer, db.ForeignKey('artists.id'), nullable=False)
-
-    song = db.relationship('Song', back_populates='song_artists')
-    artist = db.relationship('Artist', back_populates='song_artists')
-
+    song_artists = db.relationship('Song_Artist', cascade="all, delete", back_populates='song')
 
 class User(db.Model):
     __tablename__ = 'users'
@@ -80,8 +55,8 @@ class User(db.Model):
     spotify_access_token = db.Column(db.String)
     spotify_refresh_token = db.Column(db.String)
     # Defines many to many relationship through favorite table with artists
-    favorites = db.relationship('Favorite', back_populates='user')
-    user_festivals = db.relationship('User_Festival', back_populates='user')
+    favorites = db.relationship('Favorite', cascade="all, delete", back_populates='user')
+    user_festivals = db.relationship('User_Festival', cascade="all, delete", back_populates='user')
 
     # formatted for bcrypt hashing
     _password_hash = db.Column(db.String, nullable=False)
@@ -98,14 +73,38 @@ class User(db.Model):
     def authenticate(self, password):
         return bcrypt.check_password_hash(self._password_hash, password)
 
+class Lineup(db.Model):
+    __tablename__ = 'lineups'
+    # Ensure that all Artist Festival combinations are unique within the join table
+    __table_args__ = (UniqueConstraint('festival_id', 'artist_id', name='unique_lineup'),)
+
+    id = db.Column(db.Integer, primary_key=True)
+    festival_id = db.Column(db.Integer, db.ForeignKey('festivals.id', ondelete='CASCADE'), nullable=False)
+    artist_id = db.Column(db.Integer, db.ForeignKey('artists.id', ondelete='CASCADE'), nullable=False)
+
+    festival = db.relationship('Festival', back_populates='lineup')
+    artist = db.relationship('Artist', back_populates='lineup')
+
+class Song_Artist(db.Model):
+    __tablename__ = 'song_artists'
+    # Ensure all combinations of Artists and songs are unique to prevent duplicates
+    __table_args__ = (UniqueConstraint('song_id', 'artist_id', name='unique_collaboration'),)
+
+    id = db.Column(db.Integer, primary_key=True)
+    song_id = db.Column(db.Integer, db.ForeignKey('songs.id', ondelete='CASCADE'), nullable=False)
+    artist_id = db.Column(db.Integer, db.ForeignKey('artists.id', ondelete='CASCADE'), nullable=False)
+
+    song = db.relationship('Song', back_populates='song_artists')
+    artist = db.relationship('Artist', back_populates='song_artists')
+
 class Favorite(db.Model):
     __tablename__ = 'favorites'
     # Unique constraint to prevent duplicate favorites
     __table_args__ = (UniqueConstraint('user_id', 'artist_id', name='unique_favorite'),)
 
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    artist_id = db.Column(db.Integer, db.ForeignKey('artists.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id', ondelete='CASCADE'), nullable=False)
+    artist_id = db.Column(db.Integer, db.ForeignKey('artists.id', ondelete='CASCADE'), nullable=False)
 
     user = db.relationship('User', back_populates='favorites')
     artist = db.relationship('Artist', back_populates='favorites')
@@ -115,8 +114,8 @@ class User_Festival(db.Model):
     __table_args__ = (UniqueConstraint('user_id', 'festival_id', name='unique_rsvp'),)
 
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    festival_id = db.Column(db.Integer, db.ForeignKey('festivals.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id', ondelete='CASCADE'), nullable=False)
+    festival_id = db.Column(db.Integer, db.ForeignKey('festivals.id', ondelete='CASCADE'), nullable=False)
 
     user = db.relationship('User', back_populates='user_festivals')
     festival = db.relationship('Festival', back_populates='user_festival')
