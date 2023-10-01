00b1f4eb5a8899c9c976e9b7ae713b50a431f2fe
Stanley Shrewsbery <homestanrunner@gmail.com>
2023-10-01 14:40:49 -0400

    implements controlled signup form using formik

---
+++ client/src/components/Signup.js
@@ -0,0 +1,149 @@
+import React, { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom"
+import { useFormik } from "formik";
+import * as yup from "yup";
+
+
+export const SignupForm = () => {
+
+    
+    const history = useHistory()
+
+
+  const formSchema = yup.object().shape({
+    email: yup.string().email("Invalid email").required("Must enter email"),
+    firstName: yup.string().required("Must enter a name").max(15),
+    lastName: yup.string().required("Must enter a name").max(15),
+    username: yup.string().required("Must enter a username").max(20),
+    password: yup.string().required("Must enter a password"),
+    confirmPassword: yup
+      .string()
+      .label("confirm password")
+      .required("Please re-type your password").oneOf([yup.ref("password"), null], "Passwords must match"),
+  });
+
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      username: "",
+      firstName: "",
+      lastName: "",
+      password: "",
+      confirmPassword: "",
+    },
+    validationSchema: formSchema,
+    onSubmit: (values) => {
+        fetch("v1/users", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(values, null, 2),
+        })
+        .then((res) => res.json())
+        .then((data) => {
+          if (data && data.id) {
+            sessionStorage.setItem('userID', data.id);
+            sessionStorage.setItem('first_name', data.first_name);
+            sessionStorage.setItem('last_name', data.last_name);
+            sessionStorage.setItem('username', data.username);
+    
+            history.push('/');
+          }
+          else if (data && data.error) {
+            console.error(data.error);
+          }
+        })
+        .catch((err) => {
+          console.error("There was an error with the registration:", err);
+        });
+      }
+    });
+
+  return (
+    <div>
+      <h1>Signup Form</h1>
+      <form onSubmit={formik.handleSubmit}>
+        <label htmlFor="email">Email Address</label>
+        <br />
+        <input
+          type="email"
+          id="email"
+          name="email"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur('email')}
+          value={formik.values.email}
+        />
+        <p style={{ color: "red" }}>
+            {formik.touched.email && formik.errors.email ? formik.errors.email : null}
+        </p>
+        <label htmlFor="firstName">First Name</label>
+        <br />
+        <input
+          type="text"
+          id="firstName"
+          name="firstName"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur('firstName')}
+          value={formik.values.firstName}
+        />
+        <p style={{ color: "red" }}>
+            {formik.touched.firstName && formik.errors.firstName ? formik.errors.firstName : null}
+        </p>
+        <label htmlFor="lastName">Last Name</label>
+        <br />
+        <input
+          type="text"
+          id="lastName"
+          name="lastName"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur('lastName')}
+          value={formik.values.lastName}
+        />
+        <p style={{ color: "red" }}>
+            {formik.touched.lastName && formik.errors.lastName ? formik.errors.lastName : null}
+        </p>
+        <label htmlFor="username">Username</label>
+        <br />
+        <input
+          type="text"
+          id="username"
+          name="username"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur('username')}
+          value={formik.values.username}
+        />
+        <p style={{ color: "red" }}>
+            {formik.touched.username && formik.errors.username ? formik.errors.username : null}
+        </p>
+        <label htmlFor="password">Password</label>
+        <br />
+        <input
+          type="password"
+          id="password"
+          name="password"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur('password')}
+          value={formik.values.password}
+        />
+        <p style={{ color: "red" }}>
+            {formik.touched.password && formik.errors.password ? formik.errors.password : null}
+        </p>
+        <label htmlFor="confirmPassword">Confirm Password</label>
+        <br />
+        <input
+          type="password"
+          id="confirmPassword"
+          name="confirmPassword"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur('confirmPassword')}
+          value={formik.values.confirmPassword}
+        />
+        <p style={{ color: "red" }}>
+            {formik.touched.confirmPassword && formik.errors.confirmPassword ? formik.errors.confirmPassword : null}
+        </p>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
