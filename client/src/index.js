8e571ceedb699cc16c6d14317ce0559e80a724f1
Stanley Shrewsbery <homestanrunner@gmail.com>
2023-10-01 07:52:18 -0400

    Revises imports

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import App from "./App";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom/cjs/react-router-dom.min";
 
 const container = document.getElementById("root");
