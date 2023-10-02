5b380d976e84b48c5273fbeefce364fd6e288903
Stanley Shrewsbery <homestanrunner@gmail.com>
2023-10-02 05:57:05 -0400

    adds AppProvider wrapper for context

--- client/src/App.js
+++ client/src/App.js
@@ -3,29 +3,43 @@ import { Switch, Route } from "react-router-dom";
 import "./App.css"
 import NavBar from "./components/NavBar"
 import Dashboard from "./components/Dashboard"
-import FavoriteArtists from "./components/FavoriteArtists";
+import FavoriteArtists from "./components/ArtistSearch";
 import MyFestivals from "./components/MyFestivals";
 import Profile from "./components/Profile";
+import Login from "./components/Login";
+import { SignupForm } from "./components/Signup"
+import AppProvider from "./context/AppProvider";
+
+const APILink = "http://localhost:5555"
+
 
 function App() {
   return (
-      <div>
-        <NavBar />
-        <Switch>
-          <Route exact path="/">
-            <Dashboard />
-          </Route>
-          <Route path="/favorites">
-            <FavoriteArtists />
-          </Route>
-          <Route path="/festivals">
-            <MyFestivals />
-          </Route>
-          <Route path="/profile">
-            <Profile />
-          </Route>
-        </Switch>
-      </div>
+      <AppProvider>
+        <div>
+          <NavBar />
+          <Switch>
+            <Route path = "/login">
+              <Login />
+            </Route>
+            <Route path = "/signup">
+              <SignupForm />
+            </Route>
+            <Route exact path="/">
+              <Dashboard />
+            </Route>
+            <Route path="/favorites">
+              <FavoriteArtists />
+            </Route>
+            <Route path="/festivals">
+              <MyFestivals />
+            </Route>
+            <Route path="/profile">
+              <Profile />
+            </Route>
+          </Switch>
+        </div>
+      </AppProvider>
   )
 }
 
