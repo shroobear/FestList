be636238e797fc2ee885fada13b008a7bb67a39c
Stanley Shrewsbery <homestanrunner@gmail.com>
2023-10-02 13:19:51 -0400

    wraps entire App component in AppProvider

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,14 @@ import App from "./App";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom/cjs/react-router-dom.min";
+import AppProvider from "./context/AppProvider";
 
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
 root.render(
     <Router>
-        <App />
+        <AppProvider>
+            <App />
+        </AppProvider>
     </Router>
 );
