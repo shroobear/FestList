807d3970fbf9ea02c1b8fe71fe17b4836d5a5cdb
Stanley Shrewsbery <homestanrunner@gmail.com>
2023-10-04 16:24:07 -0400

    Incorporates artist deletion from lineup

--- client/src/components/EditFestival.js
+++ client/src/components/EditFestival.js
@@ -3,84 +3,120 @@ import { useParams } from "react-router-dom";
 import { Container, Row, Card, Button } from "react-bootstrap";
 import ArtistSearch from "./ArtistSearch";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { checkIfArtistExists, createArtist, checkIfInLineup, addToLineup } from "./helpers";
+import {
+  checkIfArtistExists,
+  createArtist,
+  checkIfInLineup,
+  addToLineup,
+} from "./helpers";
 
 function EditFestival() {
   const { festival_id } = useParams();
   const [currentFestival, setCurrentFestival] = useState(null);
-  const [lineup, setLineup] = useState([])
-
-  const fetchLineup = async () => {
-    try {
-        const res = await fetch(`/v1/festivals/${festival_id}/lineup`);
-        const data = await res.json();
-        setLineup(data);
-    } catch (error) {
-        console.error("Error fetching lineup:", error);
-    }
-  };
-
-  async function festivalSetter() {
-    const response = await fetch(`/v1/festivals/${festival_id}`);
-    if (response.ok) {
-        const data = await response.json();
-        console.log("data from async: ", data)
-        return setCurrentFestival(data)
-    }
-    throw new Error("Festival not found")
-  }
+  const [lineup, setLineup] = useState([]);
+  const [isEditMode, setIsEditMode] = useState(false);
 
   useEffect(() => {
-    festivalSetter();
-  }, [festival_id])
+    async function fetchData() {
+      try {
+        const festivalResponse = await fetch(`/v1/festivals/${festival_id}`);
+        const festivalData = await festivalResponse.json();
+        setCurrentFestival(festivalData);
 
-  useEffect(() => {
-    fetchLineup();
-  }, [festival_id])
+        const lineupResponse = await fetch(`/v1/festivals/${festival_id}/lineup`);
+        const lineupData = await lineupResponse.json();
+        setLineup(lineupData);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+    fetchData();
+  }, [festival_id]);
 
   async function handleAddToFestival(artistData, festival_id) {
     try {
       let artist = await checkIfArtistExists(artistData.name);
       if (!artist) {
-        console.log(artistData.id, artistData.name)
         artist = await createArtist(artistData);
       }
-  
+
       const lineupEntry = await checkIfInLineup(festival_id, artist.id);
       if (!lineupEntry) {
         await addToLineup(festival_id, artist.id);
         alert("Artist successfully added to festival!");
+        const newLineupResponse = await fetch(`/v1/festivals/${festival_id}/lineup`);
+        const newLineupData = await newLineupResponse.json();
+        setLineup(newLineupData);
       } else {
         alert("Artist is already in the lineup.");
       }
-      fetchLineup();
     } catch (error) {
       console.error("Error:", error);
       alert("Failed operation.");
     }
   }
 
+  async function handleDeleteFromLineup(festivalId, artistId) {
+    try {
+      const response = await fetch(`/v1/lineups/pair/${festivalId}/${artistId}`, {
+        method: "DELETE",
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        if (data.success) {
+          alert("Artist successfully removed from festival!");
+          const remainingArtists = lineup.filter((a) => a.id !== artistId);
+          setLineup(remainingArtists);
+        } else {
+          alert(data.message || "Failed to remove artist.");
+        }
+      } else {
+        throw new Error("Failed to delete artist from lineup.");
+      }
+    } catch (error) {
+      console.error("Error deleting artist from lineup:", error);
+      alert("Error deleting artist from lineup. Please try again.");
+    }
+  }
+
   return (
     <main>
-      <Container style={{width: "20rem"}} className="align-content-center">
+      <Container style={{ width: "20rem" }} className="align-content-center">
         <h1 className="page-header">
           {currentFestival ? currentFestival.name : "Loading..."}
         </h1>
-        <Row></Row>
         <Row>
           <Card>
             <h2>Lineup</h2>
             <ul>
-                {lineup.map((artist) => (
-                  <li key={artist.id}>{artist.name}</li>  
-                ))}
+              {lineup.map((artist) => (
+                <li key={artist.id}>
+                  {artist.name}
+                  {isEditMode && (
+                    <Button
+                      variant="light"
+                      size="sm"
+                      onClick={() => handleDeleteFromLineup(currentFestival.id, artist.id)}
+                    >
+                      ❌
+                    </Button>
+                  )}
+                </li>
+              ))}
             </ul>
-            <Button variant="outline-danger" className="mb-2" >Edit Lineup</Button>
+            <Button
+              variant="outline-danger"
+              className="mb-2"
+              onClick={() => setIsEditMode(!isEditMode)}
+            >
+              Edit Lineup
+            </Button>
           </Card>
         </Row>
       </Container>
       <Container className="mb-5">
-        <ArtistSearch onAddToFestival={handleAddToFestival} festival_id={festival_id}/>
+        <ArtistSearch onAddToFestival={handleAddToFestival} festival_id={festival_id} />
       </Container>
     </main>
   );
