38a424e1bba02d079c8700ee63d4ff248804e871
Stanley Shrewsbery <homestanrunner@gmail.com>
2023-10-04 10:45:23 -0400

    adds RSVP functionality

--- client/src/components/LineupModal.js
+++ client/src/components/LineupModal.js
@@ -1,21 +1,63 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Modal, Button, Row } from "react-bootstrap";
 
 function LineupModal({ show, setShow, currentFestival, lineup, setLineup }) {
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+  const [hasRSVPed, setHasRSVPed] = useState(false);
+
+  useEffect(() => {
+    const user_id = sessionStorage.getItem('user_id');
+    console.log("current festival:", currentFestival);
+
+    fetch(`/v1/users/${user_id}/rsvps`)
+    .then(res => res.json())
+    .then(data => {
+        console.log("RSVPs data for user:", data);
+        const rsvpForCurrentFestival = data.find(rsvp => String(rsvp.festival_id) === String(currentFestival.id));
+
+        if(rsvpForCurrentFestival) {
+            setHasRSVPed(true);
+        } else {
+            setHasRSVPed(false);
+        }
+    })
+    .catch(error => {
+      console.error('Error checking RSVP status:', error);
+    });
+  }, [currentFestival]);
 
   useEffect(() => {
     if (currentFestival) {
       fetch("/v1/festivals/" + currentFestival.id + "/lineup")
-      .then ((res) => res.json())
+      .then((res) => res.json())
       .then((data) => {
-        console.log(data)
+        console.log(data);
         setLineup(data);
         handleShow();
       });
     }
-  }, [currentFestival])
+  }, [currentFestival]);
+
+  function handleRSVPClick() {
+    const rsvpData = {
+      user_id: sessionStorage.getItem('user_id'),
+      festival_id: currentFestival.id
+    };
+
+    fetch("/v1/rsvps", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(rsvpData),
+    })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      setHasRSVPed(true);
+    });
+  }
 
   return (
     <main>
@@ -40,8 +82,8 @@ function LineupModal({ show, setShow, currentFestival, lineup, setLineup }) {
             })}
         </Modal.Body>
         <Row className="mx-4 my-3 justify-content-center gap-1">
-          <Button variant="outline-primary">RSVP</Button>
-          <Button variant="outline-primary">Generate Playlist</Button>
+          {hasRSVPed ? <Button variant="outline-primary" disabled>See you soon!</Button> : <Button variant="outline-primary" onClick={handleRSVPClick}>RSVP</Button>}
+          <Button variant="outline-primary">Generate Playlist *coming soon*</Button>
         </Row>
       </Modal>
     </main>
